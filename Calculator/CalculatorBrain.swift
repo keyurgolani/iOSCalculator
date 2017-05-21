e0bac44aaebb66621e15e388a3bd412435d9837d
Keyur Golani <keyurrgolani@gmail.com>
2017-05-21 13:12:14 -0700

    Final Commit

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,78 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Keyur Golani on 4/29/17.
+//  Copyright © 2017 Keyur Golani. All rights reserved.
+//
+
+import Foundation
+
+func multiply(op1: Double, op2: Double) -> Double {
+    return op1 * op2
+}
+
+class CalculatorBrain {
+    
+    private var accumulator = 0.0
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+    var operations: Dictionary<String, Operation> = [
+        "π": Operation.Constant(Double.pi),
+        "e": Operation.Constant(M_E),
+        "√": Operation.UnaryOperation(sqrt),
+        "cos": Operation.UnaryOperation(cos),
+        "±": Operation.UnaryOperation({ -$0 }),
+        "×": Operation.BinaryOperation({ $0 * $1 }),
+        "÷": Operation.BinaryOperation({ $0 / $1 }),
+        "+": Operation.BinaryOperation({ $0 + $1 }),
+        "-": Operation.BinaryOperation({ $0 - $1 }),
+        "=": Operation.Equals
+    ]
+    
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = PendingOperationInfo(function: function, firstOperand: accumulator)
+            case .Equals:
+                executePendingBinaryOperation()
+            }
+        }
+    }
+    
+    func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.function(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: PendingOperationInfo?
+    
+    private struct PendingOperationInfo {
+        var function: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+}
